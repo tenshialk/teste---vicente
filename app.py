d230fcb2cd7fc53401f317a3bc97b1e0e26c4e23
tenshialk <mariacicera0981@gmail.com>
2025-05-29 01:26:57 +0000

    22:26

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from models.mensagem import db  
 from controller.mensagem import mensagem_bp
 
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 # Define a URL do banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mensagens.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inicializa o banco de dados com a aplicação Flask
 db.init_app(app)
@@ -13,17 +14,13 @@ db.init_app(app)
 # Registra o Blueprint mensagem_bp na aplicação
 app.register_blueprint(mensagem_bp)
 
-mensagens=[
-    { 
-        "nome" :"roberta", "id": 1,"mensagem":"ola"     
-    },
-    
-]
-
-
+@app.route('/mensagem', methods=['GET'])
+def mensagem():
+    nome = request.args.get('nome', 'Mundo')
+    return jsonify({'mensagem': f'Olá, {nome}!'})
 
 with app.app_context():
     db.create_all()
 
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
--- controller/mensagem.py
+++ controller/mensagem.py
@@ -1,41 +1,37 @@
 from flask import Blueprint, request, jsonify
 from models.mensagem import Mensagem, db 
 
-# Cria um Blueprint chamado mensagem_bp
 mensagem_bp = Blueprint('mensagem', __name__)
-
 
 @mensagem_bp.route('/mensagens', methods=['POST'])
 def criar_mensagem():
-    dados = request.get_json()  # Pega os dados enviados pelo cliente
-    nova_mensagem = Mensagem(conteudo=dados['conteudo'])  # Cria nova instância
-    db.session.add(nova_mensagem)  # Adiciona ao banco de dados
-    db.session.commit()  # Salva a operação
-    return jsonify(nova_mensagem), 201  # Retorna a nova mensagem criada
+    dados = request.get_json()  
+    nova_mensagem = Mensagem(conteudo=dados['conteudo']) 
+    db.session.add(nova_mensagem)  
+    db.session.commit() 
+    return jsonify({'id': nova_mensagem.id, 'conteudo': nova_mensagem.conteudo}), 201  
 
 @mensagem_bp.route('/mensagens', methods=['GET'])
 def listar_mensagens():
-    mensagens = Mensagem.query.all()  # Busca todas as mensagens
-    return jsonify([{'id': m.id, 'conteudo': m.conteudo} for m in mensagens])  # Retorna em JSON
-
+    mensagens = Mensagem.query.all() 
+    return jsonify([{'id': m.id, 'conteudo': m.conteudo} for m in mensagens])  
 
 @mensagem_bp.route('/mensagens/<int:id>', methods=['GET'])
 def obter_mensagem(id):
-    mensagem = Mensagem.query.get_or_404(id)  # Busca a mensagem pelo ID
-    return jsonify({'id': mensagem.id, 'conteudo': mensagem.conteudo})  # Retorna a mensagem encontrada
-
+    mensagem = Mensagem.query.get_or_404(id) 
+    return jsonify({'id': mensagem.id, 'conteudo': mensagem.conteudo}) 
 
 @mensagem_bp.route('/mensagens/<int:id>', methods=['PUT'])
 def atualizar_mensagem(id):
-    mensagem = Mensagem.query.get_or_404(id)  # Busca a mensagem pelo ID
-    dados = request.get_json()  # Pega o novo conteúdo
-    mensagem.conteudo = dados['conteudo']  # Atualiza a mensagem
-    db.session.commit()  # Salva a alteração
-    return jsonify({'id': mensagem.id, 'conteudo': mensagem.conteudo})  # Retorna a mensagem atualizada
+    mensagem = Mensagem.query.get_or_404(id)  
+    dados = request.get_json() 
+    mensagem.conteudo = dados['conteudo'] 
+    db.session.commit() 
+    return jsonify({'id': mensagem.id, 'conteudo': mensagem.conteudo})
 
 @mensagem_bp.route('/mensagens/<int:id>', methods=['DELETE'])
 def deletar_mensagem(id):
     mensagem = Mensagem.query.get_or_404(id) 
     db.session.delete(mensagem) 
     db.session.commit() 
-    return jsonify({'mensagem': 'Mensagem deletada com sucesso.'})  # Retorna mensagem de sucesso
+    return jsonify({'mensagem': 'Mensagem deletada com sucesso.'})
